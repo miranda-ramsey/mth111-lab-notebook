d58e1f4d0b0b6b48438fd6a30c22e3ece813e3da
Miranda Ramsey <miranda.j.ramsey@gmail.com>
2025-07-03 14:47:04 -0700

    start first section

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -10,229 +10,148 @@ var ptx_lunr_docs = [
   "body": "  Math Department Portland Community College     copyright         "
 },
 {
-  "id": "ws-functions-and-function-notation",
+  "id": "sec-functions-and-function-notation",
   "level": "1",
-  "url": "ws-functions-and-function-notation.html",
+  "url": "sec-functions-and-function-notation.html",
   "type": "Subsection",
   "number": "1.1",
   "title": "Functions and Function Notation",
-  "body": " Functions and Function Notation   In this section, we’ll develop our understanding of functions and function notation, whether the function is presented as a set of ordered pairs, a table of values, a graph, or an equation. We’ll also learn how to evaluate a function given an input value and to solve for an input given a function’s output value.    Textbook Reference  This relates to content in ...    Preparation Exercises    Preparation 1   Many of us have at least one restaurant we would love to go to for a meal. Go online and find a menu for a restaurant you'd choose to eat at.    What is the name of the restaurant and what type of food do they serve?    What is a dish you would like to get and how much does it cost?    Are there any other dishes on the menu that cost the same as your dish? If so, what are they?    If I ask you the price of any specific dish from the menu, do you know how much it costs?    If you know how much I paid for a dish, do you know exactly what I ordered based just on the price? Explain your answer.       Practice Exercises    Practice 1    Determine if each of the following relations show as a function of .  Explain your reasoning by referncing the definition of a function. If a relation is not a function provide a specific example why the definition was not satisfied.  Assume all ordered pairs are of the form .     {  }                                INSERT GRAPH HERE  INSERT GRAPH HERE      Practice 2   Let be defined as the set of ordered pairs:    Find .  Solve .    Practice 3   INSERT GRAPH HERE   Find .  Solve .    Practice 4   Let    Find .  Solve .      Definitions   Relation   A relation is a set of ordered pairs.   The variable of is called the independent variable or input variable .  Each individual -value is refered to as a input or input value .    The variable of is called the dependent variable or output variable .  Each individual -value is refered to as a output or output value .     Relation  The set is a relation.     Function   A function is a relation where each possible input value is paired with exactly one output value. We say, \"The output is a function of the input,\" and often write this algebraically as .    Function  The set is a function.    Not a function  The set is a relation, but not a function.     Domain and Range    The Domain of a relation or function is the set of all possible input values.  The Range of a relation or function is the set of all possible output values.     Domain and Range  Given the function the domain of the function is and the range of the function is .     Vertical Line Test   If a vertical line can be drawn that intersects the graph more than once, the graph is not the graph of a function with as the independent variable and as the dependent variable.    Does not pass  INSERT GRAPH HERE    Passes  INSERT GRAPH HERE      Exit Exercises    Exit 1  What is the definition of a function?  What are two examples of functions that you use in your daily life outside of school? Explain how these are functions, referencing the definition of a function.  What do you look for in the graph of a relation to determine if the graph is the graph of a function or not? Fully explain your answer.    Exit 2  If , evaluate .    Exit 3  If , solve .     "
+  "body": " Functions and Function Notation   In this section, we’ll develop our understanding of functions and function notation, whether the function is presented as a set of ordered pairs, a table of values, a graph, or an equation. We’ll also learn how to evaluate a function given an input value and to solve for an input given a function’s output value.    Textbook Reference  This relates to content in Section 3.1 and Section 3.2 of Algebra and Trigonometry 2e .    Preparation 1   Many of us have at least one restaurant we would love to go to for a meal. Go online and find a menu for a restaurant you'd choose to eat at.     What is the name of the restaurant and what type of food do they serve?      What is a dish you would like to get and how much does it cost?      Are there any other dishes on the menu that cost the same as your dish? If so, what are they?      If I ask you the price of any specific dish from the menu, do you know how much it costs?      If you know how much I paid for a dish, do you know exactly what I ordered based just on the price? Explain your answer.      Practice 1    Determine if each of the following relations show as a function of .  Explain your reasoning by referncing the definition of a function. If a relation is not a function provide a specific example why the definition was not satisfied.  Assume all ordered pairs are of the form .      {  }                                         INSERT GRAPH HERE     Practice 2   a   Let be defined as the set of ordered pairs:     Find .    Solve .     b   INSERT GRAPH HERE    Find .    Solve .     c   Let     Find .    Solve .      "
 },
 {
-  "id": "prep-functions-and-function-notation-2-1-3",
+  "id": "prep-functions-and-function-notation-3",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#prep-functions-and-function-notation-2-1-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1.1",
+  "url": "sec-functions-and-function-notation.html#prep-functions-and-function-notation-3",
+  "type": "Exercise",
+  "number": "1.1.a",
   "title": "",
-  "body": " What is the name of the restaurant and what type of food do they serve?  "
+  "body": "  What is the name of the restaurant and what type of food do they serve?   "
 },
 {
-  "id": "prep-functions-and-function-notation-2-1-4",
+  "id": "prep-functions-and-function-notation-4",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#prep-functions-and-function-notation-2-1-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1.2",
+  "url": "sec-functions-and-function-notation.html#prep-functions-and-function-notation-4",
+  "type": "Exercise",
+  "number": "1.1.b",
   "title": "",
-  "body": " What is a dish you would like to get and how much does it cost?  "
+  "body": "  What is a dish you would like to get and how much does it cost?   "
 },
 {
-  "id": "prep-functions-and-function-notation-2-1-5",
+  "id": "prep-functions-and-function-notation-5",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#prep-functions-and-function-notation-2-1-5",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1.3",
+  "url": "sec-functions-and-function-notation.html#prep-functions-and-function-notation-5",
+  "type": "Exercise",
+  "number": "1.1.c",
   "title": "",
-  "body": " Are there any other dishes on the menu that cost the same as your dish? If so, what are they?  "
+  "body": "  Are there any other dishes on the menu that cost the same as your dish? If so, what are they?   "
 },
 {
-  "id": "prep-functions-and-function-notation-2-1-6",
+  "id": "prep-functions-and-function-notation-6",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#prep-functions-and-function-notation-2-1-6",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1.4",
+  "url": "sec-functions-and-function-notation.html#prep-functions-and-function-notation-6",
+  "type": "Exercise",
+  "number": "1.1.d",
   "title": "",
-  "body": " If I ask you the price of any specific dish from the menu, do you know how much it costs?  "
+  "body": "  If I ask you the price of any specific dish from the menu, do you know how much it costs?   "
 },
 {
-  "id": "prep-functions-and-function-notation-2-1-7",
+  "id": "prep-functions-and-function-notation-7",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#prep-functions-and-function-notation-2-1-7",
-  "type": "Worksheet Exercise",
-  "number": "1.1.1.5",
+  "url": "sec-functions-and-function-notation.html#prep-functions-and-function-notation-7",
+  "type": "Exercise",
+  "number": "1.1.e",
   "title": "",
-  "body": " If you know how much I paid for a dish, do you know exactly what I ordered based just on the price? Explain your answer.  "
+  "body": "  If you know how much I paid for a dish, do you know exactly what I ordered based just on the price? Explain your answer.   "
 },
 {
-  "id": "practice-functions-and-function-notation-2-1-3",
+  "id": "practice-1-functions-and-function-notation-3",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-2-1-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.1",
+  "url": "sec-functions-and-function-notation.html#practice-1-functions-and-function-notation-3",
+  "type": "Exercise",
+  "number": "1.1.a",
   "title": "",
-  "body": " {  }  "
+  "body": "  {  }   "
 },
 {
-  "id": "practice-functions-and-function-notation-2-1-4",
+  "id": "practice-1-functions-and-function-notation-4",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-2-1-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.2",
+  "url": "sec-functions-and-function-notation.html#practice-1-functions-and-function-notation-4",
+  "type": "Exercise",
+  "number": "1.1.b",
   "title": "",
-  "body": "                            "
+  "body": "                              "
 },
 {
-  "id": "practice-functions-and-function-notation-2-1-5",
+  "id": "practice-1-functions-and-function-notation-5",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-2-1-5",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.3",
+  "url": "sec-functions-and-function-notation.html#practice-1-functions-and-function-notation-5",
+  "type": "Exercise",
+  "number": "1.1.c",
   "title": "",
-  "body": "INSERT GRAPH HERE "
+  "body": "    "
 },
 {
-  "id": "practice-functions-and-function-notation-2-1-6",
+  "id": "practice-1-functions-and-function-notation-6",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-2-1-6",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.4",
+  "url": "sec-functions-and-function-notation.html#practice-1-functions-and-function-notation-6",
+  "type": "Exercise",
+  "number": "1.1.d",
   "title": "",
-  "body": "INSERT GRAPH HERE "
+  "body": " INSERT GRAPH HERE  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-1-3",
+  "id": "practice-2-functions-and-function-notation-2-3",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-1-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.5",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-2-3",
+  "type": "Exercise",
+  "number": "1.1.i",
   "title": "",
-  "body": "Find . "
+  "body": " Find .  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-1-4",
+  "id": "practice-2-functions-and-function-notation-2-4",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-1-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.6",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-2-4",
+  "type": "Exercise",
+  "number": "1.1.ii",
   "title": "",
-  "body": "Solve . "
+  "body": " Solve .  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-2-3",
+  "id": "practice-2-functions-and-function-notation-3-3",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-2-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.7",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-3-3",
+  "type": "Exercise",
+  "number": "1.1.i",
   "title": "",
-  "body": "Find . "
+  "body": " Find .  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-2-4",
+  "id": "practice-2-functions-and-function-notation-3-4",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-2-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.8",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-3-4",
+  "type": "Exercise",
+  "number": "1.1.ii",
   "title": "",
-  "body": "Solve . "
+  "body": " Solve .  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-3-3",
+  "id": "practice-2-functions-and-function-notation-4-3",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-3-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.9",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-4-3",
+  "type": "Exercise",
+  "number": "1.1.i",
   "title": "",
-  "body": "Find . "
+  "body": " Find .  "
 },
 {
-  "id": "practice-functions-and-function-notation-3-3-4",
+  "id": "practice-2-functions-and-function-notation-4-4",
   "level": "2",
-  "url": "ws-functions-and-function-notation.html#practice-functions-and-function-notation-3-3-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.2.10",
+  "url": "sec-functions-and-function-notation.html#practice-2-functions-and-function-notation-4-4",
+  "type": "Exercise",
+  "number": "1.1.ii",
   "title": "",
-  "body": "Solve . "
-},
-{
-  "id": "definitions-functions-and-function-notation-2",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#definitions-functions-and-function-notation-2",
-  "type": "Definition",
-  "number": "1.1",
-  "title": "Relation.",
-  "body": " Relation   A relation is a set of ordered pairs.   The variable of is called the independent variable or input variable .  Each individual -value is refered to as a input or input value .    The variable of is called the dependent variable or output variable .  Each individual -value is refered to as a output or output value .     Relation  The set is a relation.   "
-},
-{
-  "id": "definitions-functions-and-function-notation-3",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#definitions-functions-and-function-notation-3",
-  "type": "Definition",
-  "number": "1.3",
-  "title": "Function.",
-  "body": " Function   A function is a relation where each possible input value is paired with exactly one output value. We say, \"The output is a function of the input,\" and often write this algebraically as .    Function  The set is a function.    Not a function  The set is a relation, but not a function.   "
-},
-{
-  "id": "definitions-functions-and-function-notation-4",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#definitions-functions-and-function-notation-4",
-  "type": "Definition",
-  "number": "1.6",
-  "title": "Domain and Range.",
-  "body": " Domain and Range    The Domain of a relation or function is the set of all possible input values.  The Range of a relation or function is the set of all possible output values.     Domain and Range  Given the function the domain of the function is and the range of the function is .   "
-},
-{
-  "id": "definitions-functions-and-function-notation-5",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#definitions-functions-and-function-notation-5",
-  "type": "Definition",
-  "number": "1.8",
-  "title": "Vertical Line Test.",
-  "body": " Vertical Line Test   If a vertical line can be drawn that intersects the graph more than once, the graph is not the graph of a function with as the independent variable and as the dependent variable.    Does not pass  INSERT GRAPH HERE    Passes  INSERT GRAPH HERE   "
-},
-{
-  "id": "exit-functions-and-function-notation-2-1-2",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#exit-functions-and-function-notation-2-1-2",
-  "type": "Worksheet Exercise",
-  "number": "1.1.4.1",
-  "title": "",
-  "body": "What is the definition of a function? "
-},
-{
-  "id": "exit-functions-and-function-notation-2-1-3",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#exit-functions-and-function-notation-2-1-3",
-  "type": "Worksheet Exercise",
-  "number": "1.1.4.2",
-  "title": "",
-  "body": "What are two examples of functions that you use in your daily life outside of school? Explain how these are functions, referencing the definition of a function. "
-},
-{
-  "id": "exit-functions-and-function-notation-2-1-4",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#exit-functions-and-function-notation-2-1-4",
-  "type": "Worksheet Exercise",
-  "number": "1.1.4.3",
-  "title": "",
-  "body": "What do you look for in the graph of a relation to determine if the graph is the graph of a function or not? Fully explain your answer. "
-},
-{
-  "id": "exit-functions-and-function-notation-2-2-2",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#exit-functions-and-function-notation-2-2-2",
-  "type": "Worksheet Exercise",
-  "number": "1.1.4.4",
-  "title": "",
-  "body": "If , evaluate . "
-},
-{
-  "id": "exit-functions-and-function-notation-2-3-2",
-  "level": "2",
-  "url": "ws-functions-and-function-notation.html#exit-functions-and-function-notation-2-3-2",
-  "type": "Worksheet Exercise",
-  "number": "1.1.4.5",
-  "title": "",
-  "body": "If , solve . "
+  "body": " Solve .  "
 },
 {
   "id": "exponential-and-logarithmic",
